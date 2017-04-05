c617c63e75dd42a9865286cb7530f4c157f6fa6f
Richard Southwell <Richard Southwell>
2017-04-05 15:03:13 +0100

    Chose more sensible selection ojive, and made more comparative plots

--- R/Experimental Code/BasicTestSingleWhiting.R
+++ R/Experimental Code/BasicTestSingleWhiting.R
@@ -17,12 +17,11 @@ library("plot3Drgl")
 
 params_data <- read.csv("./vignettes/NS_species_params.csv")
 params_data$sel_func <- "sigmoid_length"
-params_data$l25 <- c(7.6, 9.8, 8.7, 10.1, 11.5, 19.8, 16.4, 19.8, 11.5,
-                     19.1, 13.2, 35.3)
-#params_data$l50 <- c(8.1, 11.8, 12.2, 20.8, 17.0, 29.0, 25.8, 29.0, 17.0,
-#                     24.3, 22.9, 43.6)
-params_data$l50 <- c(8.1, 11.8, 12.2, 20.8, 17.0, 21, 25.8, 29.0, 17.0,
+params_data$l25 <- c(7.6, 9.8, 8.7, 10.1, 11.5, 26.0, 16.4, 19.8, 11.5,
+                                          19.1, 13.2, 35.3)
+params_data$l50 <- c(8.1, 11.8, 12.2, 20.8, 17.0, 29.0, 25.8, 29.0, 17.0,
                      24.3, 22.9, 43.6)
+
 params_data$a <- c(0.007, 0.001, 0.009, 0.002, 0.010, 0.006, 0.008, 0.004,
                    0.007, 0.005, 0.005, 0.007)
 params_data$b <- c(3.014, 3.320, 2.941, 3.429, 2.986, 3.080, 3.019, 3.198,
@@ -45,7 +44,7 @@ params <- MizerParams(params_data[6,], interaction = inter, no_w = 200, min_land
                       fraction_discarded = 1, predation_multiplier = multip, intrinsic_annihilation = intri)
 sim <- project(params, effort = 1, t_max = years_run, dt = 0.1, t_save = 1)
 nDisc <- sim@n[years_run+1,1,]
-#plot(sim)
+plot(sim)
 
 plot(x=params@w, y=sim@n_d[years_run,], type="b",log="xy",xlab = "Size (g)", ylab="Abundance Of Dead Fish")
 plot(x=params@w, y=sim@n_d[years_run+1,], type="b",log="xy",xlab = "Size (g)", ylab="Abundance Of Dead Fish")
@@ -66,17 +65,20 @@ params_without_discarding <- MizerParams(params_data[6,], interaction = inter, n
 sim_without_discarding <- project(params_without_discarding, effort = 1, t_max = years_run, dt = 0.1, t_save = 1)
 nNoDisc <- sim_without_discarding@n[years_run+1,1,]
 # 
+plot(sim_without_discarding)
 
+plot(x=params@w, y=(nDisc-nNoDisc), type="b",log="xy",xlab = "Size (g)", ylab="Increase In Abundance From Discarding")
 
-#plot(x=params@w, y=(nDisc - nNoDisc), type="b",log="xy",xlab = "Size (g)", ylab="Increase In Abundance From Discarding")
+plot(x=params@w, y=nNoDisc, type="l",log="xy",xlab = "Size (g)", ylab="Whiting Abundance")
+lines(x=params@w, y=nDisc, col ="red")
 
-plot(x=params@w, y=nNoDisc, type="b",log="xy",xlab = "Size (g)", ylab="Whiting Abundance")
-lines(x=params@w, y=nDisc)
-
-sum(nDisc - nNoDisc <10)
+sum(nDisc - nNoDisc <1)
 
 plot(x=params@w[(1:30)], y=sim@n_d[years_run,(1:30)], type="b",log="y",xlab = "Size (g)", ylab="log(Abundance Of Dead Fish)")
 
 # plot change in abundance due to discarding
 # plot change in biomass due to discarding
+nDisc[1]
+
+plot(x=params@w, y=log(nDisc/nNoDisc), type="b",log="x",xlab = "Size (g)", ylab="Increase In Abundance From Discarding")
 
