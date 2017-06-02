863ca1f78e824d1700a70a38f3d680c36b37d9e4
Richard <richardsouthwell254@gmail.com>
2017-06-02 09:24:20 +0100

    .

--- R/Experimental Code/FishingParameterEstimation.R
+++ R/Experimental Code/FishingParameterEstimation.R
@@ -27,14 +27,16 @@ ptm <- proc.time()
 sim <- project(params, effort = 1, t_max = 10, dt = 0.1, t_save = 1)
 Tsim <- (proc.time() - ptm)[[3]]
 
-plot(sim)
+#plot(sim)
 
 # test different starting points
 
 n<- sim@n[11, , ]
+nn<- sim@n[11, , ]
+
 dim(n)
 
-plot(n[1,],log="xy")
+#plot(n[1,],log="xy")
 
 
 sim2 <- project(params, effort = 0.5, t_max = 100, dt = 0.1, t_save = 1, initial_n = n
@@ -45,7 +47,8 @@ plot(sim2)
 
 head(n[1,])
 
-n[1,] <- runif(length(n[1,]),0,1)
+#n[1,] <- runif(length(n[1,]),0,1)
+n[1,] <- 2*nn[2,]
 
 sim3 <- project(params, effort = 0.5, t_max = 100, dt = 0.1, t_save = 1, initial_n = n
 )
