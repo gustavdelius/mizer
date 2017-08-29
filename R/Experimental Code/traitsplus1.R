03f6ba049ca5fd40e7a4ddf5ffb683987753a69d
Richard <richardsouthwell254@gmail.com>
2017-08-29 10:04:10 +0100

    Added whiting by changing trait based parameters by name

--- R/Experimental Code/traitsplus1.R
+++ R/Experimental Code/traitsplus1.R
@@ -19,32 +19,34 @@ no_traits <- 10
 
 params <- set_trait_model(no_sp = no_traits+1, min_w_inf = 10, max_w_inf = 1e5)
 
-params@species_params[no_traits+1,] <- NS_params@species_params[6,]
+#params@species_params[no_traits+1,] <- NS_params@species_params[6,]
 
-params@species_params[no_traits+1,]
-
-NS_params@species_params[6,]
-
-#for (i in (1:dim(params@species_params)[2])){
-for (i in (2:10)){
-  
-  params@species_params[no_traits+1,i] <- NS_params@species_params[6,i]
-  
-}
-i <- 12
-params@species_params[no_traits+1,i] <- NS_params@species_params[6,i]
-i <- 14
-params@species_params[no_traits+1,i] <- NS_params@species_params[6,i]
-i <- 15
-params@species_params[no_traits+1,i] <- NS_params@species_params[6,i]
-i <- 16
-params@species_params[no_traits+1,i] <- NS_params@species_params[6,i]
-i <- 17
-params@species_params[no_traits+1,i] <- NS_params@species_params[6,i]
-i <- 18
-params@species_params[no_traits+1,i] <- NS_params@species_params[6,i]
 
 sim <- project(params, t_max=76, effort = 0)
 plot(sim)
 
+params@species_params$species
 # add mackrel
+
+
+params2 <- params
+params2@species_params$w_inf[no_traits+1] <- NS_params@species_params$w_inf[6]
+params2@species_params$w_mat[no_traits+1] <- NS_params@species_params$w_mat[6]
+params2@species_params$h[no_traits+1] <- NS_params@species_params$h[6]
+params2@species_params$gamma[no_traits+1] <- NS_params@species_params$gamma[6]
+params2@species_params$ks[no_traits+1] <- NS_params@species_params$ks[6]
+params2@species_params$beta[no_traits+1] <- NS_params@species_params$beta[6]
+params2@species_params$sigma[no_traits+1] <- NS_params@species_params$sigma[6]
+params2@species_params$z0[no_traits+1] <- NS_params@species_params$z0[6]
+params2@species_params$alpha[no_traits+1] <- NS_params@species_params$alpha[6]
+params2@species_params$sel_func[no_traits+1] <- NS_params@species_params$sel_func[6]
+params2@species_params$knife_edge_size[no_traits+1] <- NS_params@species_params$knife_edge_size[6]
+params2@species_params$gear[no_traits+1] <- NS_params@species_params$gear[6]
+params2@species_params$erepro[no_traits+1] <- NS_params@species_params$erepro[6]
+params2@species_params$k[no_traits+1] <- NS_params@species_params$k[6]
+params2@species_params$w_min[no_traits+1] <- NS_params@species_params$w_min[6]
+params2@species_params$w_min_idx[no_traits+1] <- NS_params@species_params$w_min_idx[6]
+params2@species_params$r_max[no_traits+1] <- NS_params@species_params$r_max[6]
+
+sim <- project(params2, t_max=7, effort = 0)
+plot(sim)
