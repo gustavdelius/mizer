de19d728b4f66452a67d889acb57ef5f4ece8ab2
Richard <richardsouthwell254@gmail.com>
2017-09-20 17:42:40 +0100

    got ns + small running with proper fishing efforts and interaction matrix

--- R/Experimental Code/plussmall5.R
+++ R/Experimental Code/plussmall5.R
@@ -82,6 +82,13 @@ prim <- paramsK@species_params[c("species","w_inf","w_mat","beta","sigma","r_max
 prim$k_vb <- kvb
 
 ######################################
+Fmat3 <- Fmat
+# make fishing effort matrix
+eff <- 0
+hybrid_Fmat <- matrix(eff, nrow=(no_traits+12), ncol=ncol(Fmat3))
+rownames(hybrid_Fmat) <- prim_mod$species
+colnames(hybrid_Fmat) <- colnames(Fmat3)
+
 
 # add in whiting
 prim_mod <- prim
@@ -94,48 +101,82 @@ for (i in (1:12)){
   prim_mod$sigma[rp] <- params_data$sigma[i]
   prim_mod$r_max[rp] <- params_data$r_max[i]
   prim_mod$k_vb[rp] <- params_data$k_vb[i]
+  hybrid_Fmat[rp,] <- Fmat3[i,]
+  
 }
 
 params_prim_mod_check <- MizerParams(prim_mod)
 sim_prim_mod_check <- project(params_prim_mod_check,effort = 1)
 plot(sim_prim_mod_check)
 
-# make fishing effort matrix
-eff <- 0
-hybrid_Fmat <- matrix(eff, nrow=(no_traits+1), ncol=ncol(Fmat3))
-hybrid_Fmat[rp,] <- Fmat3[whiting_no,]
-rownames(hybrid_Fmat) <- prim_mod$species
-colnames(hybrid_Fmat) <- colnames(Fmat3)
+plotSpectra(sim_prim_mod_check)
+nn <- sim_prim_mod_check@n
+ww <- sim_prim_mod_check@params@w
 
+plot(ww,nn[101,11,]*ww,log="xy")
 
 whiting_landings <- landings[whiting_no,41:(dim(landings)[2]-1)]
-
+#standard_interaction <- mean(inter)
+standard_interaction <- 1
+biginter <- matrix(standard_interaction,nrow=22,ncol=22)
+for (i in (1:12)){
+  for (j in (1:12)){
+    biginter[no_traits+i,no_traits+j] <- inter[i,j]
+  }
+}
+rownames(hybrid_Fmat) <- prim_mod$species
+colnames(hybrid_Fmat) <- colnames(Fmat3)
 
 dosim <- function(capacity=10^(11),rmax_trait=1.655195e+08,rmax_whiting=5.480000e+11){
   params_data_altered <- prim_mod
   params_data_altered$r_max[1:(rp-1)] <- rmax_trait*prim_mod$r_max[1:(rp-1)]/min(prim_mod$r_max[1:(rp-1)])
   params_data_altered$r_max[rp] <- rmax_whiting
-  params_prim_mod_altered <- MizerParams(params_data_altered,kappa=capacity)
+  params_prim_mod_altered <- MizerParams(params_data_altered,
+                                         kappa=capacity,w_pp_cutoff=1,interaction = biginter)
   #hybrid_Fmat2 <- cbind(hybrid_Fmat,hybrid_Fmat,hybrid_Fmat,hybrid_Fmat)
   #colnames(hybrid_Fmat2) <- 1990:2073
   #sim_prim_mod_altered_warmup <- project(params_prim_mod_altered,effort = t(hybrid_Fmat2))
   #sim_prim_mod_altered <- project(params_prim_mod_altered,effort = t(hybrid_Fmat),initial_n=sim_prim_mod_altered_warmup@n[dim(sim_prim_mod_altered_warmup@n)[1],,],initial_n_pp=sim_prim_mod_altered_warmup@n_pp[dim(sim_prim_mod_altered_warmup@n_pp)[1],])
-  sim_prim_mod_altered <- project(params_prim_mod_altered,effort = 1,t_max=200)
+  #sim_prim_mod_altered <- project(params_prim_mod_altered,effort = 1,t_max=200)
+  sim_prim_mod_altered <- project(params_prim_mod_altered,effort = t(hybrid_Fmat))
   return(sim_prim_mod_altered)
 }
 
 
-#plot(dosim(10^(11),1.655195e+08,5.480000e+11))
-
-#plot(dosim(10^(11),1.655195e+07,5.480000e+11))
-
-simplussmall <- dosim(10^(11),1.655195e+06,5.480000e+11)
-plot(simplussmall)
-
-moresimplussmall <- dosim(10^(11),1.655195e+07,5.480000e+11)
-plot(moresimplussmall)
-
-moresimplussmall2 <- dosim(10^(11),1.655195e+05,5.480000e+11)
+moresimplussmall2 <- dosim(10^(11),1.655195e+08,5.480000e+11)
 plot(moresimplussmall2)
-moresimplussmall2@params@species_params
+ps <- plotSpectra(moresimplussmall2)
+
+plot(moresimplussmall2@params@w_full,moresimplussmall2@params@w_full*moresimplussmall2@n_pp[201,],log="xy",ylim=c(10^(-5),10^20))
+for (i in (1:22)){
+  lines(moresimplussmall2@params@w,moresimplussmall2@params@w*moresimplussmall2@n[201,i,],log="xy",ylim=c(10^(-5),10^20))
+}
+
+
+#moresimplussmall2@params@species_params
 #plot(getBiomass(moresimplussmall2)[201,],log="y")
+#gb2 <- getBiomass(moresimplussmall2,max_w=1)
+#gb2[201,]
+#getRDI(moresimplussmall2@params, moresimplussmall2@n[201,,], moresimplussmall2@n_pp[201,])
+#getSSB(moresimplussmall2)[201,]
+#gg <- getEGrowth(moresimplussmall2@params, moresimplussmall2@n[201,,], moresimplussmall2@n_pp[201,])
+#plot(moresimplussmall2@params@w,gg[13,],log="xy")
+#plot(moresimplussmall2@params@w,getFeedingLevel(moresimplussmall2)[201,13,],log="xy")
+#plot(moresimplussmall2@params@w,moresimplussmall2@n[201,12,],log="xy",ylim=c(10^5,10^14))
+
+plot(moresimplussmall2@params@w_full,moresimplussmall2@n_pp[201,],log="xy",ylim=c(10^(-5),10^20))
+for (i in (1:22)){
+  lines(moresimplussmall2@params@w,moresimplussmall2@n[201,i,],log="xy",ylim=c(10^(-5),10^20))
+}
+gm2 <- getM2(moresimplussmall2@params,moresimplussmall2@n[201,,],moresimplussmall2@n_pp[201,])
+plot(moresimplussmall2@params@w,gm2[13,],log="x")
+
+
+# if one has a system where the plankton lines up, 
+# and one scales the k0 by a particular factor then one should 
+# change the kappa_R be the same factor in order to make things work.
+
+
+#########
+
+# cut back the plankton spectrum, and setup the new species so that it takes up the same biomass
