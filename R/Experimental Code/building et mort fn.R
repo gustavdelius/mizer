658c400e8b2bf6caebaaca8ec58934e236e3e8c3
Richard <richardsouthwell254@gmail.com>
2017-11-29 15:45:02 +0000

    got solution with prey switching included

--- R/Experimental Code/building et mort fn.R
+++ R/Experimental Code/building et mort fn.R
@@ -89,6 +89,14 @@ paramsConst <- set_trait_model(no_sp = 10, min_w_inf = 10,n=2/3,q=0.8,eta=0.25,
                                k0=10^(50),kappa=0.4, alpha=0.17,
                                h=1614.363,f0=0.5,ks=0,z0=0,gamma=660.2633
 )
+source("R/Experimental Code/project_methods_much_switch.R")
+epsi <- 0.5
+library(mizer)
+
+sim <- project(paramsConst, t_max=150, effort = 0)
+plot(sim)
+
+
 lines(paramsConst@w,paramsConst@mu_et) 
 
 plot(paramsConst@w,paramsConst@mu_et,log="x") 
@@ -103,3 +111,17 @@ lines(paramsConst@w,paramsConst@mu_et)
 
 lines(wvec,etmort)
 
+##################
+##################
+library(mizer)
+library(plyr)
+
+paramsConst <- set_trait_model(no_sp = 10, min_w_inf = 10,n=2/3,q=0.8,eta=0.25,
+                               k0=10^(50),kappa=10^(50), alpha=0.17,
+                               h=1614.363,f0=0.5,ks=0,z0=0,gamma=660.2633
+)
+epsi <- 0.5
+source("R/Experimental Code/project_methods_much_switch.R")
+
+sim <- project(paramsConst, t_max=150, effort = 0)
+plot(sim)
