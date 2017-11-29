ee9662dec9f915979c25ade60b8e2dc9d31be32c
Richard <richardsouthwell254@gmail.com>
2017-11-29 09:07:25 +0000

    got exact form for eternal predation mortality rate

--- R/Experimental Code/eternalpredators.R
+++ R/Experimental Code/eternalpredators.R
@@ -14,3 +14,15 @@ mort_eter <- function(w){
 plot(wvec,sapply(wvec,mort_eter),log="x")
 etmort <- realmu_pp-mortvals
 lines(wvec,etmort)
+
+#etmortgood[w_] := (-beta^(1 - lambda + q))*
+#  E^((1/2)*(1 - lambda + q)^2*sigma^2)*(-1 + fbar)*gamma*kappa*
+#  Sqrt[Pi/2]*sigma*w^(1 - lambda + q)*
+#  (Sqrt[1/sigma^2]*sigma + 
+#     Erf[((1 - lambda + q)*sigma^2 + Log[beta] + Log[w] - 
+#            Log[W])/(Sqrt[2]*sigma)])
+
+# implement and plot the above
+# where variables go through mizer
+# grid point maker
+# increasingly accurate 
