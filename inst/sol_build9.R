80516cbe3730d34e179c0a2c96e1417eff89797d
Gustav Delius <gustav.delius@gmail.com>
2018-01-11 09:54:20 +0000

    Calculating cc_pp earlier fixes problem with wildly different erepros

--- inst/sol_build9.R
+++ inst/sol_build9.R
@@ -10,14 +10,12 @@ library(mizer)
 #' ### Set parameters 
 f0 <- 0.6
 alpha <- 0.4
-r_pp <- 1  # Choosing a high value because we want the plankton to stay
-# at its power-law steady state
+r_pp <- 1  # Not used because we commented out the plankton dynamics
 n <- 2/3
 p <- n
 q <- 0.95
 lambda <- 2+q-n
-erepro <- 0.1
-R <- 1e10  # The rate of reproduction
+erepro <- 0.1 # Will be changed later
 
 beta <- 100
 sigma <- 1.3
@@ -38,8 +36,6 @@ min_w <- min(w_min)
 max_w <- max(w_inf)
 no_w <- log10(max_w/min_w)*100+1
 min_w_pp <- 1e-8
-# gamma was set up in a wierd way in analytic_numerical_comparison, and calulated via Mizerparams
-#gamma <- 4.999819e-11
 
 ########## preliminary params creation for gamma setup ########
 
@@ -65,31 +61,6 @@ params <- MizerParams(species_params, p=p, n=n, q=q, lambda = lambda, f0 = f0,
                       chi = 0)
 
 gamma <- params@species_params$gamma[1]
-##########
-
-
-species_params <- data.frame(
-  species = 1:no_sp,
-  w_min = w_min,
-  w_inf = w_inf,
-  w_mat = w_mat,
-  h = h,
-  ks = ks,
-  beta = beta,
-  sigma = sigma,
-  z0 = 0,
-  alpha = alpha,
-  gamma = gamma,
-  erepro = erepro,
-  sel_func = "knife_edge", # not used but required
-  knife_edge_size = 1000
-)
-
-params <- MizerParams(species_params, p=p, n=n, q=q, lambda = lambda, f0 = f0,
-                      kappa = kappa, min_w = min_w, max_w = max_w, no_w = no_w, 
-                      min_w_pp = min_w_pp, w_pp_cutoff = max_w, r_pp = r_pp,
-                      chi = 0)
-#gamma <- params@species_params$gamma[1]
 w <- params@w
 
 
@@ -141,6 +112,20 @@ for (i in 1:no_sp) {
 }
 lines(w, colSums(n_output), col="red")
 
+plankton_vec <- (kappa*w^(-lambda))-colSums(n_output)
+plankton_vec[plankton_vec<0] <- 0
+plankton_vec[min(which(plankton_vec==0)):length(plankton_vec)] <- 0
+params@cc_pp[sum(params@w_full<=w[1]):length(params@cc_pp)] <- plankton_vec
+
+m2 <- getM2(params, n_output, params@cc_pp)
+
+for (i in 1:no_sp) {
+    params@psi[i, ] <- (w/w_inf[i])^(1-n)
+    params@psi[i, w < w_mat[i]] <- 0
+    params@psi[i, w > w_inf[i]] <- 1
+    params@mu_b[i, ] <- mu0 * w^(n-1) - m2[i,]
+}
+
 # set erepro to meet boundary condition
 rdi <- getRDI(params, n_output, params@cc_pp)
 gg <- getEGrowth(params, n_output, params@cc_pp)
@@ -156,55 +141,17 @@ for (i in (1:no_sp)){
   erepro_final[i] <- erepro*(n_output[i,params@species_params$w_min_idx[i]]*(gg0+DW*mumu0))/rdi[i]
 }
 
-n_output[i,params@species_params$w_min_idx[i]]*(gg0+DW*mumu0)
-
 params@species_params$erepro <- erepro_final
-
-# check boundary condition is now satisfied
-rdi <- getRDI(params, n_output, params@cc_pp)
-gg <- getEGrowth(params, n_output, params@cc_pp)
-erepro_final <- rdi
-rdi_wanted <- rdi
-for (i in (1:no_sp)){
-  #rdi_wanted[i] <- gg[i,params@species_params$w_min_idx[i]]*n_output[i,params@species_params$w_min_idx[i]]
-  
-  gg0 <- gg[i,params@species_params$w_min_idx[i]]
-  mumu0 <- mumu[i,params@species_params$w_min_idx[i]]
-  DW <- params@dw[params@species_params$w_min_idx[i]]
-  rdi_wanted[i] <-n_output[i,params@species_params$w_min_idx[i]]*(gg0+DW*mumu0)
-  
-}
-rdi_wanted/rdi
 
 ############
 
-m2 <- getM2(params, n_output, params@cc_pp)
-
-
-params2 <- params
-
-for (i in 1:no_sp) {
-  params2@psi[i, ] <- (w/w_inf[i])^(1-n)
-  params2@psi[i, w < w_mat[i]] <- 0
-  params2@psi[i, w > w_inf[i]] <- 1
-  params2@mu_b[i, ] <- mu0 * w^(n-1) - m2[i,]
-}
-#params2@cc_pp <- (kappa*w^(-lambda))-colSums(n_output)
-#params2@cc_pp[params2@cc_pp<0] <- 0
-plankton_vec <- (kappa*w^(-lambda))-colSums(n_output)
-plankton_vec[plankton_vec<0] <- 0
-plankton_vec[min(which(plankton_vec==0)):length(plankton_vec)] <- 0
-params2@cc_pp[sum(params2@w_full<=w[1]):length(params2@cc_pp)] <- plankton_vec
-
-params2@srr <- function(rdi, species_params) {return(rdi)}
-
-sim <- project(params2, t_max=100 ,effort = 0, initial_n = n_output)
+params@srr <- function(rdi, species_params) {return(rdi)}
+params@chi <- 0.0
+sim <- project(params, t_max=300 ,effort = 0, initial_n = n_output)
 plot(sim)
 
-params@w_full[sum(params@w_full<=w[1])]
-
-plot(params2@w_full, params2@w_full*params2@cc_pp, type="l", log="xy")
-lines(params2@w_full, params2@w_full*sim@n_pp[dim(sim@n)[1],], col="blue")
+plot(params@w_full, params@w_full*params@cc_pp, type="l", log="xy")
+lines(params@w_full, params@w_full*sim@n_pp[dim(sim@n)[1],], col="blue")
 
 ##############
 
@@ -212,9 +159,9 @@ nn <- n_output
 nn[nn==0] <- 1
 nn
 
-params2@chi <- 0.05
-params2@ddd <- nn^(params2@chi)
-sim <- project(params2, t_max=300 ,effort = 0, initial_n = n_output)
+params@chi <- 0.05
+params@ddd <- nn^(params@chi)
+sim <- project(params, t_max=300 ,effort = 0, initial_n = n_output)
 plot(sim)
 
 #' This code looks like it stablizes the community at close to the input. The overall community 
@@ -239,7 +186,7 @@ for (i in (1:11)){
 ########## growth curves ###############
 
 speci <- 1
-gNS <- getEGrowth(params2, sim@n[dim(sim@n)[1],,], sim@n_pp[dim(sim@n_pp)[1],])[speci,]
+gNS <- getEGrowth(params, sim@n[dim(sim@n)[1],,], sim@n_pp[dim(sim@n_pp)[1],])[speci,]
 
 g_fnNS <- approxfun(w, gNS)
 myodefunNS <- function(t, state, parameters){
@@ -247,13 +194,13 @@ myodefunNS <- function(t, state, parameters){
 }
 ageNS <- (0:30)
 library(deSolve)
-weightNS <- ode(y = params2@species_params$w_min[speci], times = ageNS, func = myodefunNS, parms = 1)[,2]
+weightNS <- ode(y = params@species_params$w_min[speci], times = ageNS, func = myodefunNS, parms = 1)[,2]
 plot(ageNS,weightNS)
 
 
 ####################################
 
-params2@species_params$erepro
+params@species_params$erepro
 
 # try and change q by 0.1
 
