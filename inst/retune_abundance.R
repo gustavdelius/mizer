7b7fdc6b205409170cfdebe69d1a01d639033cd9
Richard <richardsouthwell254@gmail.com>
2018-03-23 11:29:03 +0000

    # #  #20 and #42 Changed cc to sum_i A_i[old]*N_i(w), the code works now, but # only when all the initial multipliers are 1 or NULL, since we need to know # what values to use.

--- inst/retune_abundance.R
+++ inst/retune_abundance.R
@@ -2,68 +2,80 @@
 
 
 #@ retune_abundance <- function(params,A){
-  params <- set_scaling_model()
-  A <- rep(1,length(params@species_params$w_inf))
-  A[4:7] <- NA
+params <- set_scaling_model()
+A <- rep(1,length(params@species_params$w_inf))
+A[4:7] <- NA
 
-  
-  
-  # get a list of the species that we can tune abundance mulpliers of
-  all_background <- (1:length(A))[is.na(A)]
-  largest_background <- which.max(params@species_params$w_inf[all_background])
-  A2 <- A
-  # we are assuming that the the abundance multiplier of the largest backgroud 
-  # species should be held fixed at 1, if though it was initially an NA
-  A2[largest_background] <- 1
-  
-  
-  # we make a list L of species we will vary the abundance parameters of
-  # (everything but largest background)
-  #L <- all_background[all_background!=largest_background]
-  L <- (1:length(A))[is.na(A2)]
-  idx_start <- sum(params@w<=min(params@species_params$w_mat))
-  idx_stop <- sum(params@w<=max(params@species_params$w_inf))
-  RR <- matrix(0,nrow = length(L), ncol = length(L))
-  QQ <- (1:length(L))
-  cc <- params@kappa*params@w^(-params@lambda)
-  Lcomp <- (1:length(A))[!is.na(A2)]
-  
-  old_n <- params@initial_n
-  no_sp <- length(params@species_params$w_inf)
-  
-  for (i in 1:no_sp){
-    old_n[i,] <- A2[i]*params@initial_n[i,]
-  }
-  #rho <- colSums(params@initial_n[Lcomp,])
-  rho <- colSums(old_n[Lcomp,])
-  
-  
-  for (i in (1:length(L))){
-    QQ[i] <- sum((params@initial_n[L[i],]*(cc-rho)*params@dw/(cc^2))[idx_start:(idx_stop-1)])
-    for (j in (1:length(L))){
-      RR[i,j] <- sum((params@initial_n[L[i],]*params@initial_n[L[j],]*params@dw/(cc^2))[idx_start:(idx_stop-1)])
-    }
-  }
-  
-  A2[L] <- solve(RR,QQ)
 
-  #@  return(A2)}
-  new_n <- params@initial_n
-  for (i in 1:no_sp){
-    new_n[i,] <- A2[i]*params@initial_n[i,]
+
+# get a list of the species that we can tune abundance mulpliers of
+all_background <- (1:length(A))[is.na(A)]
+largest_background <- which.max(params@species_params$w_inf[all_background])
+A2 <- A
+# we are assuming that the the abundance multiplier of the largest backgroud 
+# species should be held fixed at 1, if though it was initially an NA
+A2[largest_background] <- 1
+
+
+# we make a list L of species we will vary the abundance parameters of
+# (everything but largest background)
+#L <- all_background[all_background!=largest_background]
+L <- (1:length(A))[is.na(A2)]
+idx_start <- sum(params@w<=min(params@species_params$w_mat))
+idx_stop <- sum(params@w<=max(params@species_params$w_inf))
+RR <- matrix(0,nrow = length(L), ncol = length(L))
+QQ <- (1:length(L))
+
+Lcomp <- (1:length(A))[!is.na(A2)]
+
+old_n <- params@initial_n
+no_sp <- length(params@species_params$w_inf)
+
+A3 <- A2
+A3[is.na(A3)] <- 1
+
+for (i in 1:no_sp){
+  old_n[i,] <- A3[i]*params@initial_n[i,]
+}
+#cc <- params@kappa*params@w^(-params@lambda)
+cc <- colSums(old_n)
+#rho <- colSums(params@initial_n[Lcomp,])
+rho <- colSums(old_n[Lcomp,])
+
+
+for (i in (1:length(L))){
+  QQ[i] <- sum((params@initial_n[L[i],]*(cc-rho)*params@dw/(cc^2))[idx_start:(idx_stop-1)])
+  for (j in (1:length(L))){
+    RR[i,j] <- sum((params@initial_n[L[i],]*params@initial_n[L[j],]*params@dw/(cc^2))[idx_start:(idx_stop-1)])
   }
-  A2
-  plot(params@w,new_n[1,],log="xy", ylim=c(10^(-3),max(new_n)))
-  for (i in (2:no_sp)){
-    lines(params@w,new_n[i,])
-  }
-  
-  #  #20 and #42 Made steps to make sure abundance multipliers are used. 
-  # Finishing building retune_abundance in a seperate file. 
-  # 
-  
-  #  #20 and #42 Have debugged a few little things. The resulting code (retune_abundance.R
-  # in the adsp branch)
-  # is tested on the scale free case, with some multipliers as 1, and 
-  # some multipliers as NA. It does not work correctly,  A[5:7] = c(1.4,0.27,2.28)
-  # I expected instead that the code would set these multipliers to unity.
+}
+
+A2[L] <- solve(RR,QQ)
+
+#@  return(A2)}
+new_n <- params@initial_n
+for (i in 1:no_sp){
+  new_n[i,] <- A2[i]*params@initial_n[i,]
+}
+A2
+plot(params@w,new_n[1,],log="xy", ylim=c(10^(-3),max(new_n)))
+for (i in (2:no_sp)){
+  lines(params@w,new_n[i,])
+}
+
+plot(params@w[idx_start:(idx_stop-1)],(colSums(new_n)/(params@kappa*params@w^(-params@lambda)))[idx_start:(idx_stop-1)],log="x")
+
+
+#  #20 and #42 Made steps to make sure abundance multipliers are used. 
+# Finishing building retune_abundance in a seperate file. 
+# 
+
+#  #20 and #42 Have debugged a few little things. The resulting code (retune_abundance.R
+# in the adsp branch)
+# is tested on the scale free case, with some multipliers as 1, and 
+# some multipliers as NA. It does not work correctly,  A[5:7] = c(1.4,0.27,2.28)
+# I expected instead that the code would set these multipliers to unity.
+
+# #  #20 and #42 Changed cc to sum_i A_i[old]*N_i(w), the code works now, but 
+# only when all the initial multipliers are 1 or NULL, since we need to know 
+# what values to use.
