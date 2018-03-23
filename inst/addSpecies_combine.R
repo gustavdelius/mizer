c92aefe378581ec010ae52e20992d368fe3a8c90
Gustav Delius <gustav.delius@gmail.com>
2018-03-23 19:47:54 +0000

    Changes we made during our video chat.

--- inst/addSpecies_combine.R
+++ inst/addSpecies_combine.R
@@ -1,46 +1,51 @@
 
+
 ############# retune_abundance code for later use
 
 
-# Abundance multipliers are 
+# Abundance multipliers are
 # currently only NULL or 1.
 
-# retune the abundance multipliers. A is initial abundance multiplier, 
-# A[i] is 1 if i is a foreground species. A[i] is null otherwise
-retune_abundance <- function(params,A){
-  # get a list of the species that we can tune abundance mulpliers of
-  all_background <- (1:length(A))[is.na(A)]
-  largest_background <- which.max(params@species_params$w_inf[all_background])
-  A2 <- A
-  # we are assuming that the the abundance multiplier of the largest backgroud 
-  # species should be held fixed at 1, if though it was initially an NA.
-  A2[largest_background] <- 1
-  # we make a list L of species we will vary the abundance parameters of
-  # (everything but largest background)
-  L <- (1:length(A))[is.na(A2)]
-  idx_start <- sum(params@w<=min(params@species_params$w_mat))
-  idx_stop <- sum(params@w<=max(params@species_params$w_inf))
-  RR <- matrix(0,nrow = length(L), ncol = length(L))
-  QQ <- (1:length(L))
-  Lcomp <- (1:length(A))[!is.na(A2)]
-  old_n <- params@initial_n
-  no_sp <- length(params@species_params$w_inf)
-  cc <- colSums(params@initial_n[all_background,])
-  rho <- colSums(params@initial_n[Lcomp,])
-  den <- cc^2
-  #! is this the right way to avoid division by zero ?
-  den[den==0] <- 10^(-50)
-  for (i in (1:length(L))){
-    QQ[i] <- sum((params@initial_n[L[i],]*(cc-rho)*params@dw/(den))[idx_start:(idx_stop-1)])
-    for (j in (1:length(L))){
-      RR[i,j] <- sum((params@initial_n[L[i],]*params@initial_n[L[j],]*params@dw/(den))[idx_start:(idx_stop-1)])
+# retune the abundance multipliers. A is initial abundance multiplier,
+# A[i] is 1 if i is a foreground species. A[i] is NA otherwise
+retune_abundance <- function(params, A) {
+    # get a list of the species that we can tune abundance mulpliers of
+    all_background <- is.na(A)
+    largest_background <-
+        which.max(params@species_params$w_inf[all_background])
+    # we are assuming that the the abundance multiplier of the largest backgroud
+    # species should be held fixed at 1, if though it was initially an NA.
+    A2 <- A
+    A2[largest_background] <- 1
+    # we make a list L of species we will vary the abundance parameters of
+    # (everything but largest background)
+    L <- (1:length(A))[is.na(A2)]
+    idx_start <- sum(params@w <= min(params@species_params$w_mat))
+    idx_stop <- sum(params@w < max(params@species_params$w_inf))
+    RR <- matrix(0, nrow = length(L), ncol = length(L))
+    QQ <- (1:length(L))
+    Lcomp <- (1:length(A))[!is.na(A2)]
+    old_n <- params@initial_n
+    no_sp <- length(params@species_params$w_inf)
+    cc <- colSums(params@initial_n[all_background, ])
+    rho <- colSums(params@initial_n[Lcomp, ])
+    den <- cc ^ 2
+    for (i in (1:length(L))) {
+        QQ[i] <-
+            sum((params@initial_n[L[i], ] * (cc - rho) * params@dw / (den))[idx_start:idx_stop])
+        for (j in (1:length(L))) {
+            RR[i, j] <-
+                sum((
+                    params@initial_n[L[i], ] * params@initial_n[L[j], ] * params@dw / (den)
+                )[idx_start:idx_stop])
+        }
     }
-  }
-  A2[L] <- solve(RR,QQ)
-  if (sum(A2<0)>0){
-    stop('Abundance multipliers generated with negative entries')
-  }
-  return(A2)}
+    A2[L] <- solve(RR, QQ)
+    if (sum(A2 < 0) > 0) {
+        stop('Abundance multipliers generated with negative entries')
+    }
+    return(A2)
+}
 
 #params <- set_scaling_model()
 #A <- rep(1,length(params@species_params$w_inf))
@@ -54,147 +59,140 @@ retune_abundance <- function(params,A){
 
 #! note we are assuming theta_ij=1, for all i,j
 
-#! the code currently breaks under the default rfac=inf case, because it 
+#! the code currently breaks under the default rfac=inf case, because it
 # relies on some values of rmax being passed through
 
 #params <- set_scaling_model(rfac = 10^10)
 
 
 
-add_species <-function(params,species_params,mult=1.5*10^(11)){
-  
-  
-  if (length(species_params$r_max)==0){
-    species_params$r_max <- 10^(50)
-  }
-  
-  
-  #####################
-  
-  combi_species_params <- data.frame(
-    species = 1:(length(params@species_params$species)+length(species_params$species)),
-    w_min = c(params@species_params$w_min,species_params$w_min),
-    w_inf = c(params@species_params$w_inf,species_params$w_inf),
-    w_mat = c(params@species_params$w_mat,species_params$w_mat),
-    h = c(params@species_params$h,species_params$h),
-    ks = c(params@species_params$ks,species_params$ks),
-    beta = c(params@species_params$beta,species_params$beta),
-    sigma = c(params@species_params$sigma,species_params$sigma),
-    z0 = 0,
-    alpha = c(params@species_params$alpha,species_params$alpha),
-    erepro = c(params@species_params$erepro,species_params$erepro),
-    sel_func = "knife_edge",
-    # not used but required
-    knife_edge_size = c(params@species_params$knife_edge_size,species_params$knife_edge_size),
-    gear = c(params@species_params$gear,species_params$gear),
-    r_max = c(params@species_params$r_max,species_params$r_max)
-  )
-  
-  combi_params <-
-    MizerParams(
-      combi_species_params,
-      p = params@p,
-      n = params@n,
-      q = params@q,
-      lambda = params@lambda,
-      f0 = params@f0,
-      kappa = params@kappa,
-      min_w = min(params@w),
-      max_w = max(params@w),
-      no_w = length(params@w),
-      min_w_pp = min(params@w_full),
-      w_pp_cutoff = max(params@w_full),
-      r_pp = (params@rr_pp/(params@w_full^(params@p-1)))[1]
-    )
-  
-  
-  combi_params@initial_n_pp <- params@initial_n_pp
-  combi_params@cc_pp <- params@cc_pp
-  
-  new_sp <- length(params@species_params$species)+1
-  for (i in (1:(new_sp-1))){
-    combi_params@initial_n[i,] <- params@initial_n[i,]
-    combi_params@species_params$erepro[i] <- params@species_params$erepro[i] 
-    combi_params@psi[i,] <- params@psi[i,]
-    combi_params@mu_b[i,] <- params@mu_b[i,]
-  }
-  # other important info to pass through correspond to 
-  # the parts of params that got modified after set_scaling made it initially.
-  combi_params@species_params$erepro[new_sp] <- params@species_params$erepro[(new_sp-1)]
-  #! maybe we do not have to change psi[new_sp,]
-  combi_params@psi[new_sp,] <- (combi_params@w / combi_params@species_params$w_inf[new_sp]) ^ (1 - combi_params@n)
-  combi_params@psi[new_sp, combi_params@w < (combi_params@species_params$w_mat[new_sp] - 1e-10)] <- 0
-  combi_params@psi[new_sp, combi_params@w > (combi_params@species_params$w_inf[new_sp] - 1e-10)] <- 1
-  combi_params@mu_b[new_sp,] <- params@mu_b[(new_sp-1),]
-  #! what about params@srr ? do I have to pass this through when rmax is off ?
-  #! do I have to set rmax off if it is off in two inputs ?
-  # use rest of info to fill out n_new_sp properly
-  # combi_params@initial_n[new_sp,] <- params@initial_n[(new_sp-1),]
-  ################################
-  combi_params@interaction[new_sp,new_sp] <- 0
-  mumu <- getZ(combi_params, combi_params@initial_n, combi_params@initial_n_pp,0)[new_sp,]
-  gg <- getEGrowth(combi_params, combi_params@initial_n, combi_params@initial_n_pp)[new_sp,]
-  
-  w_inf_idx <- length(combi_params@w[combi_params@w<=combi_params@species_params$w_inf[new_sp]])
-  integrand <- params@dw[combi_params@species_params$w_min_idx[new_sp]:w_inf_idx]*mumu[
-    combi_params@species_params$w_min_idx[new_sp]:w_inf_idx]/gg[
-      combi_params@species_params$w_min_idx[new_sp]:w_inf_idx]
-  combi_params@initial_n[new_sp,] <- 0
-  #! Here we are setting the abundance multiplier to 1. More useful to set it 
-  # to the value it should be if we were adding in another scale free species.
-  combi_params@initial_n[new_sp,combi_params@species_params$w_min_idx[new_sp]:w_inf_idx] <- 
-    mult*exp(-cumsum(integrand))/gg[combi_params@species_params$w_min_idx[new_sp]:w_inf_idx]
-  #! not really sure why the above seems to create a NAN in the last weight box.
-  combi_params@initial_n[is.nan(combi_params@initial_n)] <- 0
-  
-  combi_params@interaction[new_sp,new_sp] <- 1
-  ################## reset erepro
-  #! this version readjusts erepro in the case where rmax=inf, need to think more 
-  # about what to do in general case, and look at wrapper functions.
-  gg0 <- gg[combi_params@species_params$w_min_idx[new_sp]]
-  mumu0 <- mumu[combi_params@species_params$w_min_idx[new_sp]]
-  rdi <- getRDI(combi_params, combi_params@initial_n, combi_params@initial_n_pp)[new_sp]
-  DW <- combi_params@dw[combi_params@species_params$w_min_idx[new_sp]]
-  #combi_params@species_params$erepro[new_sp] <- combi_params@species_params$erepro[new_sp]*(
-  #  combi_params@initial_n[new_sp,combi_params@species_params$w_min_idx[new_sp]]*(gg0+DW*mumu0))/rdi
-  
-  H <- rdi/combi_params@species_params$erepro[new_sp]
-  X <- combi_params@initial_n[new_sp,combi_params@species_params$w_min_idx[new_sp]]*(gg0+DW*mumu0)
-  
-  combi_params@species_params$erepro[new_sp] <- 
-    combi_params@species_params$r_max[new_sp]*X/(H*combi_params@species_params$r_max[new_sp]+H*X)
-  ##################
-  #sim <- project(combi_params, t_max=5, effort = 0)
-  #plot(sim)
-  
-  ###########
-  #A <- rep(NA,length(combi_params@species_params$w_inf))
-  A <- c(params@A,1)
-  A[length(combi_params@species_params$w_inf)] <- 1
-  
-  AA <- retune_abundance(combi_params,A)
-  new_n <- combi_params@initial_n
-  for (i in 1:length(combi_params@species_params$w_inf)){
-    new_n[i,] <- AA[i]*combi_params@initial_n[i,]
-  }
-  combi_params@initial_n <- new_n
-  combi_params@A <- c(params@A,1)
-  return(combi_params)}
+add_species <- function(params, species_params, mult = 1.5 * 10 ^ (11)) {
+
+    combi_species_params <- rbind(params@species_params, species_params)
+    if (length(combi_species_params$r_max) == 0) {
+        combi_species_params$r_max <- 10 ^ (50)
+    }
+    combi_params <-
+        MizerParams(
+            combi_species_params,
+            p = params@p,
+            n = params@n,
+            q = params@q,
+            lambda = params@lambda,
+            f0 = params@f0,
+            kappa = params@kappa,
+            min_w = min(params@w),
+            max_w = max(params@w),
+            no_w = length(params@w),
+            min_w_pp = min(params@w_full),
+            w_pp_cutoff = max(params@w_full),
+            r_pp = (params@rr_pp / (params@w_full ^ (params@p - 1)))[1]
+        )
+    
+    
+    combi_params@initial_n_pp <- params@initial_n_pp
+    combi_params@cc_pp <- params@cc_pp
+    
+    new_sp <- length(params@species_params$species) + 1
+
+    combi_params@initial_n[1:(new_sp - 1), ] <- params@initial_n
+    combi_params@species_params$erepro[1:(new_sp - 1)] <-
+        params@species_params$erepro
+    combi_params@psi[1:(new_sp - 1), ] <- params@psi
+    combi_params@mu_b[1:(new_sp - 1), ] <- params@mu_b
+    
+    # other important info to pass through correspond to
+    # the parts of params that got modified after set_scaling made it initially.
+    combi_params@species_params$erepro[new_sp] <- 0.1
+    #! maybe we do not have to change psi[new_sp,]
+    combi_params@psi[new_sp, ] <-
+        (combi_params@w / combi_params@species_params$w_inf[new_sp]) ^ (1 - combi_params@n)
+    combi_params@psi[new_sp, combi_params@w < (combi_params@species_params$w_mat[new_sp] - 1e-10)] <-
+        0
+    combi_params@psi[new_sp, combi_params@w > (combi_params@species_params$w_inf[new_sp] - 1e-10)] <-
+        1
+    combi_params@mu_b[new_sp, ] <- params@mu_b[(new_sp - 1), ]
+    #! what about params@srr ? do I have to pass this through when rmax is off ?
+    #! do I have to set rmax off if it is off in two inputs ?
+    combi_params@srr <- params@srr
+    # use rest of info to fill out n_new_sp properly
+    # combi_params@initial_n[new_sp,] <- params@initial_n[(new_sp-1),]
+    ################################
+    combi_params@interaction[new_sp, new_sp] <- 0
+    mumu <-
+        getZ(combi_params,
+             combi_params@initial_n,
+             combi_params@initial_n_pp,
+             effort = 0)[new_sp, ]
+    gg <-
+        getEGrowth(combi_params,
+                   combi_params@initial_n,
+                   combi_params@initial_n_pp)[new_sp, ]
+    
+    w_inf_idx <-
+        sum(combi_params@w < combi_params@species_params$w_inf[new_sp])
+    integrand <-
+        params@dw[combi_params@species_params$w_min_idx[new_sp]:w_inf_idx] * mumu[combi_params@species_params$w_min_idx[new_sp]:w_inf_idx] /
+        gg[combi_params@species_params$w_min_idx[new_sp]:w_inf_idx]
+    combi_params@initial_n[new_sp, ] <- 0
+
+    combi_params@initial_n[new_sp, combi_params@species_params$w_min_idx[new_sp]:w_inf_idx] <-
+        mult * exp(-cumsum(integrand)) / gg[combi_params@species_params$w_min_idx[new_sp]:w_inf_idx]
+    
+    combi_params@interaction[new_sp, new_sp] <- 1
+    ################## reset erepro
+    #! this version readjusts erepro in the case where rmax=inf, need to think more
+    # about what to do in general case, and look at wrapper functions.
+    gg0 <- gg[combi_params@species_params$w_min_idx[new_sp]]
+    mumu0 <- mumu[combi_params@species_params$w_min_idx[new_sp]]
+    rdi <-
+        getRDI(combi_params,
+               combi_params@initial_n,
+               combi_params@initial_n_pp)[new_sp]
+    DW <-
+        combi_params@dw[combi_params@species_params$w_min_idx[new_sp]]
+    #combi_params@species_params$erepro[new_sp] <- combi_params@species_params$erepro[new_sp]*(
+    #  combi_params@initial_n[new_sp,combi_params@species_params$w_min_idx[new_sp]]*(gg0+DW*mumu0))/rdi
+    
+    H <- rdi / combi_params@species_params$erepro[new_sp]
+    X <-
+        combi_params@initial_n[new_sp, combi_params@species_params$w_min_idx[new_sp]] *
+        (gg0 + DW * mumu0)
+    
+    combi_params@species_params$erepro[new_sp] <-
+        combi_params@species_params$r_max[new_sp] * X / (H * combi_params@species_params$r_max[new_sp] +
+                                                             H * X)
+    ##################
+    #sim <- project(combi_params, t_max=5, effort = 0)
+    #plot(sim)
+    
+    ###########
+    #A <- rep(NA,length(combi_params@species_params$w_inf))
+    A <- c(params@A, 1)
+    
+    AA <- retune_abundance(combi_params, A)
+    print(AA)
+    new_n <- combi_params@initial_n
+    for (i in 1:length(combi_params@species_params$w_inf)) {
+        new_n[i, ] <- AA[i] * combi_params@initial_n[i, ]
+    }
+    combi_params@initial_n <- new_n
+    combi_params@A <- c(params@A, 1)
+    return(combi_params)
+}
 
 
 params <- set_scaling_model()
 params@A[] <- NA
-if (length(params@species_params$r_max)==0){
-  params@species_params$r_max <- rep(10^(50),length(params@species_params$w_inf))
-}
 
-species_params <- params@species_params[10,]
+species_params <- params@species_params[10, ]
+species_params$species <- "foreground"
 #species_params$beta <- 50
 
-params_out <- add_species(params,species_params,mult=1.5*10^(11))
+params_out <- add_species(params, species_params, mult = 1.5 * 10 ^ (11))
 
 
-sim <- project(params_out, t_max=5, effort = 0)
+sim <- project(params_out, t_max = 5, effort = 0)
 plot(sim)
 
 #AA
@@ -205,12 +203,9 @@ plot(sim)
 #plot(params@w,colSums(new_n),log="xy")
 
 
-#20 #42 Added "abundance mulplier" slots to params, really they are binary 
+#20 #42 Added "abundance mulplier" slots to params, really they are binary
 # background indicators. Cleaned add_species code somewhat
-# (1) Is using den instead of cc^2 the right way to avoid division by zero ? (2) When negative 
-# abundance  multipliers, re-iterate the solver 
-# in case where some species are locked out. (3) Somehow improve 
+# (1) Is using den instead of cc^2 the right way to avoid division by zero ? (2) When negative
+# abundance  multipliers, re-iterate the solver
+# in case where some species are locked out. (3) Somehow improve
 # manner of recording abundance vectors. Currently A is just 1 or null.
-
-
-
