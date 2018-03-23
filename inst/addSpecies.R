492bf852be93613ee00e8096be55663055fdcdf2
Richard <richardsouthwell254@gmail.com>
2018-03-23 08:23:13 +0000

    #  #20 and #42 Got basic layout of code using linear solving to solve quadratic optimization problem of # abundance multipliers.

--- inst/addSpecies.R
+++ inst/addSpecies.R
@@ -176,26 +176,47 @@ retune_abundance <- function(params,A,N_C){
 
 ###########
 
-Y <- "list of species with free parameters"
+#A <- c(1,NA,NA,NA)
+#all_background <- (1:length(A))[is.na(A)]
+#largest_background <- 2
 
-idx_start <- sum(w<=min(w_mat[Y]))
-idx_stop <- sum(w<=max(w_inf[Y]))
+# we are assuming that the the abundance multiplier of the largest species, 
+# alth
 
-RR <- params@interaction
-# no_free*no_free matrix
-QQ <- params@species_params$gamma
-for (i in (1:no_free)){
-  QQ[i] <- sum((initial_n[i,]*kappa*params@w^(-lambda)*params@dw)[idx_start:(idx_stop-1)])
-  for (j in (1:no_sp)){
-    RR[i,j] <- sum((initial_n[i,]*initial_n[j,]*params@dw)[idx_start:(idx_stop-1)])
+# before we start, we pick out the largest baclground species, and change 
+# its abundance multiplier from NA to 1.
+retune_abundance <- function(params,A,N_C){
+  # get a list of the species that we can tune abundance mulpliers of
+  all_background <- (1:length(A))[is.na(A)]
+  largest_background <- which.max(params@species_params$w_inf[all_background])
+  A2 <- A
+  # we are assuming that the the abundance multiplier of the largest backgroud 
+  # species should be held fixed at 1, if though it was initially an NA
+  A2[largest_background] <- 1
+  
+  
+  # we make a list L of species we will vary the abundance parameters of
+  # (everything but largest background)
+  #L <- all_background[all_background!=largest_background]
+  L <- (1:length(A))[is.na(A2)]
+  idx_start <- sum(w<=min(params@species_params$w_mat[L]))
+  idx_start <- sum(w<=min(params@species_params$w_mat[L]))
+  RR <- matrix(0,nrow = length(L), ncol = length(L))
+  QQ <- (1:length(L))
+  cc <- kappa*params@w^(-params@lambda)
+  Lcomp <- (1:length(A))[!is.na(A2)]
+  rho <- colSums(params@initial_n[Lcomp,])
+
+for (i in (1:length(L))){
+  QQ[i] <- sum((params@initial_n[L[i],]*(cc-rho)*params@dw/(cc^2))[idx_start:(idx_stop-1)])
+  for (j in (1:length(L))){
+    RR[i,j] <- sum((params@initial_n[L[i],]*params@initial_n[L[j],]*params@dw/(cc^2))[idx_start:(idx_stop-1)])
   }
 }
 
-AA <- solve(RR,QQ)
-n_output_new <- initial_n
-for (i in (1:no_sp)){
-  n_output_new[i,] <- AA[i]*initial_n[i,]
-}
+A2[Y] <- solve(RR,QQ)
+
+
 
 ##############
   
@@ -253,4 +274,7 @@ for (i in (1:no_sp)){
 # and then run it again. Next job is to connect retune_abundance to the end of add_species
 
 #  #20 and #42 Building code using linear solving to solve quadratic optimization problem of 
+# abundance multipliers.
+
+#  #20 and #42 Got basic layout of code using linear solving to solve quadratic optimization problem of 
 # abundance multipliers.
