d438b25eab506141df9e1e895fc51d79763e085e
Richard <richardsouthwell254@gmail.com>
2018-03-26 11:00:59 +0100

    # #20 #42 Added mullet to code

--- inst/addSpecies_combine.R
+++ inst/addSpecies_combine.R
@@ -196,6 +196,50 @@ species_params$species <- "foreground"
 
 params_out <- add_species(params, species_params, mult = 1.5 * 10 ^ (11))
 
+params_out@species_params$erepro
+
+sim <- project(params_out, t_max = 5, effort = 0)
+plot(sim)
+
+params_out@species_params$erepro
+
+############### add mullet to set scaling ###############
+
+
+
+species_params <- data.frame(
+  species = "foreground",
+  w_min = 0.001,
+  w_inf = 251.94,
+  w_mat = 16.48,
+  h = NA, # will compute this later
+  ks = 4,
+  beta = 283,
+  sigma = 1.8,
+  z0 = 0,
+  alpha = 0.4,
+  erepro = 0.1,
+  sel_func = "knife_edge", # not used but required
+  knife_edge_size = 100,
+  gear = "knife_edge_gear",
+  k = 0,
+  gamma = NA,
+  w_min_idx = NA
+)
+
+k_vb <- 0.6
+
+species_params$h <- 3*k_vb*(species_params$w_inf^(1/3))/(species_params$alpha*params@f0)
+ae <- sqrt(2*pi) * species_params$sigma * species_params$beta^(params@lambda-2) * exp((params@lambda-2)^2 * species_params$sigma^2 / 2)
+species_params$gamma <- (species_params$h / (params@kappa * ae)) * (params@f0 / (1 - params@f0))
+species_params$w_min_idx <- sum(params@w<=species_params$w_min)
+
+# make gamma slot, then the combination should work. Note that f0 is not setup properly.
+
+#h_gurn <- 3*params_data_NS$k_vb[gurn_sp]*((params_data_NS$w_inf[gurn_sp])^(1/3))/(alpha[rep_idx]*f0)
+
+params_out <- add_species(params, species_params, mult = 5.5 * 10 ^ (9))
+
 
 sim <- project(params_out, t_max = 5, effort = 0)
 plot(sim)
@@ -218,3 +262,5 @@ params_out@species_params$erepro
 # manner of recording abundance vectors. Currently A is just 1 or null.
 
 # #20 #42 Now we retune erepro for all species at the end.
+
+# #20 #42 Added mullet to code
