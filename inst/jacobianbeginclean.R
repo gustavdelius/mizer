503e3018ac514a1cdad1a26a4a646df284c51e3a
unknown <richardsouthwell254@gmail.com>
2018-05-15 11:52:12 +0100

    #74 got newton raphson working

--- inst/jacobianbeginclean.R
+++ inst/jacobianbeginclean.R
@@ -1,27 +1,27 @@
 
 fish_rates <- function(params, n, n_pp){
-    gg<-getEGrowth(s_params, n, n_pp )
-    ggnn <- gg*n
-    RR <- getRDD(params, n, n_pp)
-    FF <- ggnn
-    ZZ <- getZ(params, n, n_pp, effort = 0)
-    for ( i in (1:dim(ggnn)[1])){
-        X <- ggnn[i,] 
-        FF[i,] <- -(X - c(RR[i],X[1:(length(X)-1)]))/params@dw - ZZ[i,]*n[i,]
-    }
-    return(FF)
+  gg<-getEGrowth(s_params, n, n_pp )
+  ggnn <- gg*n
+  RR <- getRDD(params, n, n_pp)
+  FF <- ggnn
+  ZZ <- getZ(params, n, n_pp, effort = 0)
+  for ( i in (1:dim(ggnn)[1])){
+    X <- ggnn[i,] 
+    FF[i,] <- -(X - c(RR[i],X[1:(length(X)-1)]))/params@dw - ZZ[i,]*n[i,]
+  }
+  return(FF)
 }
 
 resource_rates <- function(params, n, n_pp){
-    m2_background <- getM2Background(params, n=n, n_pp=n_pp)
-    
-    FF_resource <- params@rr_pp*(params@cc_pp-n_pp)-m2_background*n_pp
-    return(FF_resource)
+  m2_background <- getM2Background(params, n=n, n_pp=n_pp)
+  
+  FF_resource <- params@rr_pp*(params@cc_pp-n_pp)-m2_background*n_pp
+  return(FF_resource)
 }
 
 #
 
-s_params <- set_scaling_model()
+s_params <- set_scaling_model(no_sp = 2, no_w = 20)
 
 nstart <- s_params@initial_n
 nstart[1,] <- nstart[1,]
@@ -34,11 +34,11 @@ plot(sim)
 library("rootSolve")
 
 model <- function(x) {
-   F1 <- x[1] + x[2] + x[3]^2 -12
-   F2 <- x[1]^2 - x[2] + x[3] -2
-   F3 <- 2*x[1] - x[2]^2 + x[3] -1
-   c(F1 = F1, F2 = F2, F3 = F3)
-   }
+  F1 <- x[1] + x[2] + x[3]^2 -12
+  F2 <- x[1]^2 - x[2] + x[3] -2
+  F3 <- 2*x[1] - x[2]^2 + x[3] -1
+  c(F1 = F1, F2 = F2, F3 = F3)
+}
 
 
 ss <- multiroot(f = model, start = c(1, 1, 1))
@@ -52,14 +52,14 @@ ss$f.root
 results <- 1:dim(sim@n)[1]
 
 for (T in (1:dim(sim@n)[1])){
-    
-    my_n <- sim@n[T,,]
-    #my_n[1, ] <- 1000*my_n[1, ]
-    my_n_pp <- sim@n_pp[T,]
-    FF <- fish_rates(s_params, my_n, my_n_pp)
-    FF_resource <- resource_rates(s_params, my_n, my_n_pp)
-    dataout <- c(FF_resource,as.vector(FF))
-    results[T] <- max(abs(dataout))
+  
+  my_n <- sim@n[T,,]
+  #my_n[1, ] <- 1000*my_n[1, ]
+  my_n_pp <- sim@n_pp[T,]
+  FF <- fish_rates(s_params, my_n, my_n_pp)
+  FF_resource <- resource_rates(s_params, my_n, my_n_pp)
+  dataout <- c(FF_resource,as.vector(FF))
+  results[T] <- max(abs(dataout))
 }
 plot(results,type="l")
 ########################
@@ -68,13 +68,13 @@ used_params <- s_params
 change_function <- function(data_in){
   my_n_pp <- data_in[1:length(used_params@cc_pp)]
   #n <- data_in[(length(used_params@cc_pp)+1):length(data_in)]
- # matrix(vec,nrow = 7,ncol = 7)
+  # matrix(vec,nrow = 7,ncol = 7)
   my_n <- array(data_in[(length(used_params@cc_pp)+1):length(data_in)], dim = dim(used_params@initial_n))
   
-FF <- fish_rates(used_params, my_n, my_n_pp)
-FF_resource <- resource_rates(used_params, my_n, my_n_pp)
-dataout <- c(FF_resource,as.vector(FF))
-return(dataout)
+  FF <- fish_rates(used_params, my_n, my_n_pp)
+  FF_resource <- resource_rates(used_params, my_n, my_n_pp)
+  dataout <- c(FF_resource,as.vector(FF))
+  return(dataout)
 }
 
 ##########################
@@ -82,4 +82,14 @@ return(dataout)
 trial_data_in <- c(my_n_pp, as.vector(my_n))
 
 ss_go <- multiroot(f = change_function, start = trial_data_in)
+
+root_n_pp <- ss_go$root[1:length(used_params@cc_pp)]
+root_n <- array(ss_go$root[(length(used_params@cc_pp)+1):length(ss_go$root)], dim = dim(used_params@initial_n))
+
+sim <- project(used_params, t_max=15, effort = 0, initial_n = root_n, t_save = 1, initial_n_pp = root_n_pp)
+plot(sim)
+
+# by the looks of things, this steady state by newton raphson seems to involve the second species being at very low abundance
+  sim@n[16,2,]
+
 
