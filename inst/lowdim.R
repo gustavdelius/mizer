94da6b4568b1661f78581cc12646bc6753cf2f32
unknown <richardsouthwell254@gmail.com>
2018-06-01 11:15:40 +0100

    #74 coded rescaled newton raphson

--- inst/lowdim.R
+++ inst/lowdim.R
@@ -44,6 +44,36 @@ myF <- function(params,X){
   return(X-Y)
 }
 
+get_wts <- function(params){
+  return(c(params@w_full,params@species_params$w_min,params@w)^params@lambda)
+}
+
+myF_rescaled <- function(params,XX){
+  wts <- get_wts(params)
+  return(wts*myF(params,XX/wts))
+}
+
+
+params <- set_scaling_model(no_sp = 2, no_w=100)
+
+sim <- project(params, t_max=50, effort = 0, initial_n = params@initial_n, t_save = 1, initial_n_pp = params@initial_n_pp)
+plot(sim)
+
+FF <- function(XX){
+  return(myF_rescaled(params,XX))
+}
+
+
+wts <- get_wts(params)
+trial_data_in <- wts*n2RECvec(params, n_pp = params@initial_n_pp, n = params@initial_n)
+
+ss <- multiroot(f = FF, start = trial_data_in, rtol = 0, atol = 1e-15, ctol = 0)
+
+outputs <- RECvec2n(params, ss$root/wts)
+
+sim <- project(used_params, t_max=15, effort = 0, initial_n = outputs$n, t_save = 1, initial_n_pp = outputs$n_pp)
+plot(sim)
+
 
 #################################################################
 #################################################################
