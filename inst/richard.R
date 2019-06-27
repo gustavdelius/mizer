cd5ecff6488b074f07da34069dfe9ef45d228161
gustavdelius <gustav.delius@gmail.com>
2019-06-27 08:28:23 +0100

    This version is now close to that of Richard

--- inst/richard.R
+++ inst/richard.R
@@ -1,7 +1,13 @@
-plankton_logistic <- function(params, n, n_pp, B, rates, dt, ...) {
-    i <- 0.1 * params@w_full^(-params@lambda)
+plankton_logistic <- function(params, 
+                              n = params@initial_n, 
+                              n_pp = params@initial_n_pp, 
+                              B = params@initial_B, 
+                              rates = getRates(params), 
+                              dt = 0.1, ...) {
+    i <- 0.1 * params@w_full^(-params@lambda) * exp(-6.9*(lambda - 1))
     f <- params@rr_pp * n_pp * (1 - n_pp / params@cc_pp) + i - 
         rates$plankton_mort 
+    f[is.na(f)] <- 0
     return(n_pp + dt * f)
 }
 
@@ -31,7 +37,7 @@ species_params <- data.frame(
     erepro = 0.5,
     alpha = 0.1,
     ks = 0,
-    gamma = 1920 * 75000,
+    gamma = 1920,# * 75000,
     ppmr_min = 100,
     ppmr_max = 30000,
     pred_kernel_type = "norm_box",
@@ -40,17 +46,18 @@ species_params <- data.frame(
 
 
 params <- set_multispecies_model(
+    no_w = 143,
     species_params,
     lambda = lambda,
-    kappa = 1 * 10^(-3*lambda),
+    kappa = 1 * exp(-6.9*(lambda - 1)),
     w_pp_cutoff = 0.1,
     q = 0.8,
-    min_w_pp = 1e-10)
+    plankton_dynamics = plankton_logistic)
 r0 <- 1
 params@rr_pp[] <- r0 * params@w_full^(0.85 - 1)
 
 # Larval mortality
-mu_l <- 10
+mu_l <- 0
 w_l <- 0.03
 rho_l <- 5
 mu_b <- mu_l / (1 + (params@w / w_l)^rho_l)
@@ -67,12 +74,12 @@ params@interaction[] <- 0
 # initial anchovy abundance
 params@initial_n[] <- 0.001 * params@w^(-1.8)
 
-sim <- project(params)
+sim <- project(params, t_max = 2, dt = 0.01, t_save = 0.01)
 plotlySpectra(sim)
 plotlyBiomass(sim)
 
 sim <- project(sim)
 plotlyBiomass(sim)
-plotlySpectra(sim)
+plotlySpectra(sim, wlim = c(1e-10, NA), power = 1, ylim = c(NA, NA))
 
 params@w[67]
