8b14ded2ee71def05ea5205195668e754cd41b40
Gustav Delius <gustav.delius@gmail.com>
2019-09-24 11:54:36 +0100

    Script to load catch data from Google sheet and convert it into the form needed by tuneParams()

---
+++ inst/blanes/convert_catch_data.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(googlesheets)
+params <- readRDS("inst/blanes/params.rds")
+
+size_frequency_sheet <- gs_title("size-frequency")
+size_frequency <- gs_read(size_frequency_sheet)
+
+catch <- gs_title("size-frequency") %>% 
+    gs_read() %>% 
+    pivot_longer(-cm, 
+                 names_to = "latin.name", 
+                 values_to = "catch",
+                 values_drop_na = TRUE) %>% 
+    separate(cm, c("start", "end"), 
+             sep = "-",
+             convert = TRUE) %>% 
+    mutate(length = start - 0.1,
+           dl = end - length) %>% 
+    left_join(params@species_params, by = "latin.name") %>% 
+    select(species, latin.name, length, dl, catch)
+catch
+saveRDS(catch, "inst/blanes/catch.rds")
